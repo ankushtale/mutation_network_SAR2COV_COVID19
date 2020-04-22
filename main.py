3915688898c4faf114ca91510560904b9195202f
avatar178 <avatarjaykrushna@gmail.com>
2020-04-21 19:51:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def main():
     pp = pprint.PrettyPrinter(indent=4)
     polypeptide = translation(genome)
     neutral_mutations_count_per_codon = check_for_neutral_mutations(genome)
-
+    non_neutral_one_mutation_away = spike_protein_mutations(genome)
+    print(non_neutral_one_mutation_away)
     mutation_network(amino_acid_seq = "", n_mutations=15)
 
 
