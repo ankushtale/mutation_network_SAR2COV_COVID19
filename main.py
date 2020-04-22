41f336ba3e79018d4180b26b123dba23f003b369
avatar178 <avatarjaykrushna@gmail.com>
2020-04-22 02:04:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-from utils import read_file, translation, check_for_neutral_mutations, mutation_network
+from utils import read_file, translation, check_for_neutral_mutations, mutation_network, spike_protein_mutations
 import pprint
+from math import log
 
 
 def main():
     genome = read_file("wuhan")
     pp = pprint.PrettyPrinter(indent=4)
     polypeptide = translation(genome)
-    neutral_mutations_count_per_codon = check_for_neutral_mutations(genome)
-    non_neutral_one_mutation_away = spike_protein_mutations(genome)
-    print(non_neutral_one_mutation_away)
-    mutation_network(amino_acid_seq = "", n_mutations=15)
 
+    SARS_2 = "TATTTGAACGATACC"
+
+    # neutral_mutations_count_per_codon = check_for_neutral_mutations(genome)
+    mutation_list = spike_protein_mutations(SARS_2)
+
+    print(mutation_list)
+
+    mutation_network(amino_acid_seq = "", n_mutations=15)
 
 if __name__ == "__main__":
     main()
